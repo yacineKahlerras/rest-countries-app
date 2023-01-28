8b484c04bc370567ebc168614e02822045209d28
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2023-01-28 15:38:57 +0100

    ok

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {},
     colors: {
-      DarkBlue: "var(--Dark-Blue)" /**mamaaa */,
-      VeryDarkBlue: "var(--Very-Dark-Blue)",
-      VeryDarkBlue: "var(--Very-Dark-Blue)",
-      DarkGray: "var(--Dark-Gray)",
-      VeryLightGray: "var(--Very-Light-Gray)",
-      White: "var(--White)",
+      DarkBlue: "hsl(209, 23%, 22%)" /*(Dark Mode Elements)*/,
+      VeryDarkBlue: "hsl(207, 26%, 17%)" /*(Dark Mode Background)*/,
+      VeryDarkBlue: "hsl(200, 15%, 8%)" /*(Light Mode Text)*/,
+      DarkGray: "hsl(0, 0%, 52%)" /*(Light Mode Input)*/,
+      VeryLightGray: "hsl(0, 0%, 98%)" /*(Light Mode Background)*/,
+      White: "hsl(0, 0%, 100%)" /*(Dark Mode Text & Light Mode Elements)*/,
     },
   },
   plugins: [],
