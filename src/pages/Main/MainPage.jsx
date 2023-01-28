1b6eda3ed8e66088148c612582486ec520fd5d31
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2023-01-28 17:11:54 +0100

    ok

--- src/pages/Main/MainPage.jsx
+++ src/pages/Main/MainPage.jsx
@@ -3,11 +3,13 @@ import React from "react";
 function MainPage() {
   return (
     <div className="pt-8 px-5">
-      <input
-        type="text"
-        placeholder="Search for a country"
-        className="p-3 px-5 w-full max-w-xs rounded-lg dark:bg-DarkBlue drop-shadow-lg"
-      />
+      <div className=" flex p-3 px-5 w-full max-w-xs rounded-lg dark:bg-DarkBlue drop-shadow-lg">
+        <input
+          type="text"
+          placeholder="Search for a country"
+          className="focus:outline-none bg-[transparent] grow"
+        />
+      </div>
     </div>
   );
 }
