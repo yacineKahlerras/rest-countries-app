840b12186130f6740ca48b33e9725231bbe61d1b
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2023-01-30 13:56:58 +0100

    ok

--- src/pages/Main/components/CountryElement.jsx
+++ src/pages/Main/components/CountryElement.jsx
@@ -1,52 +1,10 @@
 import FilterDataContext from "@/utils/contexts/FilterDataContext";
 import React, { useContext } from "react";
+import CountryName from "./CountryName";
 
 function CountryElement(props) {
   const { country } = props;
   const { searchFilter } = useContext(FilterDataContext);
-
-  function NamePieces(str, match) {
-    str = str.toLowerCase();
-    match = match.toLowerCase();
-    var pieces = [];
-    var index,
-      pos = 0;
-
-    while ((index = str.indexOf(match, pos)) >= 0) {
-      if (pos !== index) {
-        pieces.push(str.substr(pos, index - pos));
-      }
-      pieces.push(match);
-      pos = index + match.length;
-    }
-    if (pos < str.length) {
-      pieces.push(str.substr(pos));
-    }
-    return pieces;
-  }
-
-  function CountryName() {
-    return (
-      <h2 className="font-bold text-lg mb-3">
-        {searchFilter == ""
-          ? country.name.common
-          : NamePieces(country.name.common, searchFilter).map(
-              (piece, index) => {
-                if (piece == searchFilter)
-                  return (
-                    <span
-                      key={index}
-                      className="text-DarkGray underline font-extrabold"
-                    >
-                      {piece}
-                    </span>
-                  );
-                return piece;
-              }
-            )}
-      </h2>
-    );
-  }
 
   const infos = [
     {
@@ -76,7 +34,7 @@ function CountryElement(props) {
         />
       </div>
       <div className="p-6">
-        <CountryName />
+        <CountryName searchFilter={searchFilter} country={country} />
         {infos.map((inf, infoIdx) => {
           return (
             <div key={infoIdx} className="flex gap-2">
---
+++ src/pages/Main/components/CountryName.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+function CountryName(props) {
+  const { searchFilter, country } = props;
+
+  function NamePieces(str, match) {
+    str = str.toLowerCase();
+    match = match.toLowerCase();
+    var pieces = [];
+    var index,
+      pos = 0;
+
+    while ((index = str.indexOf(match, pos)) >= 0) {
+      if (pos !== index) {
+        pieces.push(str.substr(pos, index - pos));
+      }
+      pieces.push(match);
+      pos = index + match.length;
+    }
+    if (pos < str.length) {
+      pieces.push(str.substr(pos));
+    }
+    return pieces;
+  }
+
+  return (
+    <h2 className="font-bold text-lg mb-3">
+      {searchFilter == ""
+        ? country.name.common
+        : NamePieces(country.name.common, searchFilter).map((piece, index) => {
+            if (piece == searchFilter)
+              return (
+                <span
+                  key={index}
+                  className="text-DarkGray underline font-extrabold"
+                >
+                  {piece}
+                </span>
+              );
+            return piece;
+          })}
+    </h2>
+  );
+}
+
+export default CountryName;
