fc5861f7dd42934ee4fca409f97fe7ffe2d92b61
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2023-01-31 14:29:21 +0100

    ok

--- src/pages/CountryPage/components/BorderCountries.jsx
+++ src/pages/CountryPage/components/BorderCountries.jsx
@@ -1,11 +1,39 @@
+import baseUrl from "@/utils/data/baseUrl";
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function BorderCountries(props) {
   const { countries, currentCountry, borderSigns } = props;
+  const neighbours = borderSigns
+    ? borderSigns.map((sign) => {
+        for (let i = 0; i < countries.length; i++) {
+          if (countries[i].cca3 == sign)
+            return {
+              name: countries[i].name.common,
+              linkName: countries[i].name.common
+                .replace(/\s+/g, "_")
+                .toLowerCase(),
+            };
+        }
+      })
+    : [];
 
   return (
     <div>
       <h2>border countries</h2>
+      <div className="flex gap-2 flex-wrap">
+        {neighbours.map((border, index) => {
+          return (
+            <Link
+              to={`${baseUrl}/${border.linkName}`}
+              key={index}
+              className="bg-DarkBlue p-[.3em] px-[1.3em] rounded-sm "
+            >
+              {border.name}
+            </Link>
+          );
+        })}
+      </div>
     </div>
   );
 }
