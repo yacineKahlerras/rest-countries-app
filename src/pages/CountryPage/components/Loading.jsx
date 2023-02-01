bbff743fa65002d70ca6c0d444bb088af56accc7
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2023-02-01 23:42:09 +0100

    ok

--- src/pages/CountryPage/components/Loading.jsx
+++ src/pages/CountryPage/components/Loading.jsx
@@ -1,23 +1,27 @@
 import React from "react";
-import { Triangle } from "react-loader-spinner";
+import { Triangle, Oval } from "react-loader-spinner";
 
 export default function Loading(props) {
   let { isLoading } = props;
-  isLoading = true;
+  //   isLoading = true;
 
   return (
     <div
-      className={`w-full h-full absolute top-0 left-0 place-items-center bg-VeryDarkBlue
+      className={`w-full h-full absolute top-0 left-0 place-items-center isolate 
       ${isLoading ? "grid" : "hidden"}`}
     >
-      <Triangle
-        height="80"
-        width="80"
+      <div className="w-full h-full bg-VeryLightGray dark:bg-VeryDarkBlue absolute top-0 left-0 opacity-80 -z-10"></div>
+      <Oval
+        height={80}
+        width={80}
         color="#4fa94d"
-        ariaLabel="triangle-loading"
         wrapperStyle={{}}
-        wrapperClassName=""
+        wrapperClass=""
         visible={true}
+        ariaLabel="oval-loading"
+        secondaryColor="#4fa94d"
+        strokeWidth={2}
+        strokeWidthSecondary={2}
       />
     </div>
   );
