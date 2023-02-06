fd1997f01e9329604e500199d5fa5627ae8d3616
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2023-02-06 17:42:08 +0100

    ok

--- src/pages/Main/components/SearchInput.jsx
+++ src/pages/Main/components/SearchInput.jsx
@@ -1,10 +1,16 @@
 import FilterDataContext from "@/utils/contexts/FilterDataContext";
-import React, { memo, useContext, useEffect } from "react";
+import React, { memo, useContext, useEffect, useRef } from "react";
 import { useSearchParams } from "react-router-dom";
 
 function SearchInput() {
   const { setSearchFilter } = useContext(FilterDataContext);
   let [searchParams, setSearchParams] = useSearchParams();
+  const searchBar = useRef();
+
+  useEffect(() => {
+    const urlSearch = searchParams.get("search");
+    if (urlSearch) searchBar.current.value = urlSearch;
+  }, []);
 
   function searchHandle(e) {
     setSearchFilter(e.target.value);
@@ -31,6 +37,7 @@ function SearchInput() {
         <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
       </svg>
       <input
+        ref={searchBar}
         onChange={searchHandle}
         type="text"
         placeholder="Search for a country..."
