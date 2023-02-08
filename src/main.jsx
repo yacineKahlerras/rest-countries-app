e28c3ac3ca08b6bcec696d7cb72a2d5c151f032f
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2023-02-08 17:04:19 +0100

    ok

--- src/main.jsx
+++ src/main.jsx
@@ -31,10 +31,7 @@ const router = createBrowserRouter([
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     <RouterProvider router={router} />
-//   </React.StrictMode>
-// );
