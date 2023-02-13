b1f90e1ed13d9185fd929ff6b8c39be8fbff15aa
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2023-02-13 10:09:06 +0100

    ok

--- src/components/Nav/components/NavProfile.jsx
+++ src/components/Nav/components/NavProfile.jsx
@@ -3,17 +3,18 @@ import SignOut from "@/firebase/googleSignOut";
 
 export default function NavProfile(props) {
   const { user, toggleProfileDropdown, dropDownActive } = props;
+  const dropDownVisible = dropDownActive ? "" : "hidden";
 
   const profileDropDown = (
     <div>
       <ul
         className={`absolute top-[125%] right-0 bg-VeryLightGray dark:bg-VeryDarkBlue drop-shadow-lg
-        p-3 px-5 whitespace-nowrap rounded-md flex flex-col gap-1 ${
-          dropDownActive ? "" : "hidden"
-        }`}
+        p-3 px-5 whitespace-nowrap rounded-md flex flex-col gap-1 text-left ${dropDownVisible}`}
       >
-        <li>Profile</li>
-        <li onClick={SignOut}>Sign Out</li>
+        <li className="hover:font-medium">Profile</li>
+        <li className="hover:font-medium" onClick={SignOut}>
+          Sign Out
+        </li>
       </ul>
     </div>
   );
