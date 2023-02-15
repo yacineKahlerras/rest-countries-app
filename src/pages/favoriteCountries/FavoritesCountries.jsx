306a1a183c63925ef4f2d7674e7bd4a0f27e1b4f
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2023-02-15 22:19:05 +0100

    ok

--- src/pages/favoriteCountries/FavoritesCountries.jsx
+++ src/pages/favoriteCountries/FavoritesCountries.jsx
@@ -1,12 +1,21 @@
 import FilterDataContext from "@/utils/contexts/FilterDataContext";
 import React, { useContext } from "react";
 import { useOutletContext } from "react-router-dom";
+import CountryElement from "../Main/components/CountryElement";
 
 export default function FavoriteCountries() {
   const { favoriteCountries } = useContext(FilterDataContext);
   const [countries] = useOutletContext();
+  const favoriteCountriesMap = countries.filter(
+    (country) => favoriteCountries[country.name.common]
+  );
 
-  console.log(favoriteCountries);
-
-  return <h1>Heyoooooooo</h1>;
+  return (
+    <div className="grid justify-center justify-items-center gap-5 grid-cols-countriesMap mx-auto pb-14">
+      {favoriteCountriesMap.map((favoriteCountry) => {
+        favoriteCountry.isFavorite = true;
+        return <CountryElement country={favoriteCountry} />;
+      })}
+    </div>
+  );
 }
