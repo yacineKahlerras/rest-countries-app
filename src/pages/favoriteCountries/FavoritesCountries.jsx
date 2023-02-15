e3ae7a5c6c3eabdee2887515daf0363707b88046
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2023-02-15 21:41:35 +0100

    ok

--- src/pages/favoriteCountries/FavoritesCountries.jsx
+++ src/pages/favoriteCountries/FavoritesCountries.jsx
@@ -1,5 +1,12 @@
-import React from "react";
+import FilterDataContext from "@/utils/contexts/FilterDataContext";
+import React, { useContext } from "react";
+import { useOutletContext } from "react-router-dom";
 
 export default function FavoriteCountries() {
+  const { favoriteCountries } = useContext(FilterDataContext);
+  const [countries] = useOutletContext();
+
+  console.log(favoriteCountries);
+
   return <h1>Heyoooooooo</h1>;
 }
