9520eaabe3cdeb43465e8948550da05458b4af86
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2023-02-16 15:05:22 +0100

    ok

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -5,10 +5,17 @@ import NavBarData from "@/utils/contexts/DarkThemeContext";
 import { Link } from "react-router-dom";
 import baseUrl from "@/utils/data/baseUrl";
 import NavSignIn from "./components/NavSignIn";
+import Sidemenu from "../sidemenu/Sidemenu";
 
 function Nav() {
   const { darkTheme, setDarkTheme } = useContext(NavBarData);
   const [sidemenuActive, setSidemenuActive] = useState(false);
+  const sidemenuParams = {
+    darkTheme,
+    sidemenuActive,
+    toggleTheme,
+    toggleSidemenu,
+  };
 
   function toggleTheme() {
     setDarkTheme((oldValue) => !oldValue);
@@ -19,48 +26,14 @@ function Nav() {
   }
 
   return (
-    <nav className="bg-White dark:bg-DarkBlue drop-shadow-md z-30">
-      <div className="max-w-7xl mx-auto flex justify-between items-center px-4 py-6">
-        <Link to={`${baseUrl}`}>
-          <h1>Where is the world ?</h1>
-        </Link>
-        <div className="gap-5 hidden md:flex">
-          <NavSignIn />
-          <button onClick={toggleTheme} className="flex gap-1 items-center">
-            <img
-              src={darkTheme ? sunIcon : moonIcon}
-              alt="theme"
-              className="w-4 dark:w-5 dark:invert"
-            />
-            {darkTheme ? "Light" : "Dark"} Mode
-          </button>
-        </div>
-
-        {/* sidebar button */}
-        <button
-          className={`w-9 aspect-square bg-hamburger bg-no-repeat bg-cover bg-center invert md:hidden`}
-          aria-label="sidemenu button"
-          onClick={toggleSidemenu}
-        ></button>
-      </div>
-
-      {/* sidemenu */}
-      <div
-        className={`fixed w-full h-screen top-0 left-0 bg-VeryDarkBlue overflow-x-hidden 
-        transition-all 
-        ${!sidemenuActive ? "-z-50 bg-opacity-0" : "bg-opacity-70 z-50"}`}
-      >
-        <div
-          className={`absolute h-full w-[min(100%,15rem)] bg-DarkBlue top-0 right-0 transition-all 
-          ${!sidemenuActive ? "translate-x-full" : ""}`}
-        >
-          <button
-            className={`w-9 aspect-square bg-close bg-no-repeat bg-cover bg-center invert 
-            md:hidden absolute top-5 right-5`}
-            aria-label="sidemenu button"
-            onClick={toggleSidemenu}
-          ></button>
-          <ul className=" mt-28 pl-10 flex flex-col gap-5 ">
+    <>
+      <nav className="bg-White dark:bg-DarkBlue drop-shadow-md z-30">
+        <div className="max-w-7xl mx-auto flex justify-between items-center px-4 py-6">
+          <Link to={`${baseUrl}`}>
+            <h1>Where is the world ?</h1>
+          </Link>
+          <div className="gap-5 hidden md:flex">
+            <NavSignIn />
             <button onClick={toggleTheme} className="flex gap-1 items-center">
               <img
                 src={darkTheme ? sunIcon : moonIcon}
@@ -69,11 +42,20 @@ function Nav() {
               />
               {darkTheme ? "Light" : "Dark"} Mode
             </button>
-            <NavSignIn />
-          </ul>
+          </div>
+
+          {/* sidebar button */}
+          <button
+            className={`w-9 aspect-square bg-hamburger bg-no-repeat bg-cover bg-center invert md:hidden`}
+            aria-label="sidemenu button"
+            onClick={toggleSidemenu}
+          ></button>
         </div>
-      </div>
-    </nav>
+
+        {/* sidemenu */}
+        <Sidemenu sidemenuParams={sidemenuParams} />
+      </nav>
+    </>
   );
 }
 
---
+++ src/components/sidemenu/Sidemenu.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import sunIcon from "@/assets/images/darkTheme/brightness-low-fill.svg";
+import moonIcon from "@/assets/images/darkTheme/moon-fill.svg";
+import NavSignIn from "../Nav/components/NavSignIn";
+
+export default function Sidemenu(props) {
+  const { sidemenuActive, toggleSidemenu, toggleTheme, darkTheme } =
+    props.sidemenuParams;
+
+  return (
+    <div
+      className={`fixed w-full h-screen top-0 left-0 bg-VeryDarkBlue overflow-x-hidden 
+  transition-all 
+  ${!sidemenuActive ? "-z-50 bg-opacity-0" : "bg-opacity-70 z-50"}`}
+    >
+      <div
+        className={`absolute h-full w-[min(100%,15rem)] bg-DarkBlue top-0 right-0 transition-all 
+    ${!sidemenuActive ? "translate-x-full" : ""}`}
+      >
+        <button
+          className={`w-9 aspect-square bg-close bg-no-repeat bg-cover bg-center invert 
+      md:hidden absolute top-5 right-5`}
+          aria-label="sidemenu button"
+          onClick={toggleSidemenu}
+        ></button>
+        <ul className=" mt-28 pl-10 flex flex-col gap-5 ">
+          <button onClick={toggleTheme} className="flex gap-1 items-center">
+            <img
+              src={darkTheme ? sunIcon : moonIcon}
+              alt="theme"
+              className="w-4 dark:w-5 dark:invert"
+            />
+            {darkTheme ? "Light" : "Dark"} Mode
+          </button>
+          <NavSignIn />
+        </ul>
+      </div>
+    </div>
+  );
+}
