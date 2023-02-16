a1bc82084c4c976da82db6088a64750121c72672
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2023-02-16 12:46:27 +0100

    ok

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -9,7 +9,6 @@ import NavSignIn from "./components/NavSignIn";
 function Nav() {
   const { darkTheme, setDarkTheme } = useContext(NavBarData);
   const [sidemenuActive, setSidemenuActive] = useState(false);
-  const menuBtnImg = sidemenuActive ? "bg-close" : "bg-hamburger";
 
   function toggleTheme() {
     setDarkTheme((oldValue) => !oldValue);
@@ -39,8 +38,7 @@ function Nav() {
 
         {/* sidebar button */}
         <button
-          className={`w-9 aspect-square bg-hamburger bg-no-repeat bg-cover bg-center invert 
-          md:hidden`}
+          className={`w-9 aspect-square bg-hamburger bg-no-repeat bg-cover bg-center invert md:hidden`}
           aria-label="sidemenu button"
           onClick={toggleSidemenu}
         ></button>
@@ -49,16 +47,30 @@ function Nav() {
       {/* sidemenu */}
       <div
         className={`fixed w-full h-screen top-0 left-0 bg-VeryDarkBlue overflow-x-hidden 
-        transition-all ${
-          !sidemenuActive ? "-z-50 bg-opacity-0" : "bg-opacity-70 z-30"
-        }`}
+        transition-all 
+        ${!sidemenuActive ? "-z-50 bg-opacity-0" : "bg-opacity-70 z-50"}`}
       >
         <div
-          className={`absolute h-full w-[min(100%,15rem)] bg-DarkBlue top-0 right-0 transition-all ${
-            !sidemenuActive ? "translate-x-full" : ""
-          }`}
+          className={`absolute h-full w-[min(100%,15rem)] bg-DarkBlue top-0 right-0 transition-all 
+          ${!sidemenuActive ? "translate-x-full" : ""}`}
         >
-          <ul></ul>
+          <button
+            className={`w-9 aspect-square bg-close bg-no-repeat bg-cover bg-center invert 
+            md:hidden absolute top-5 right-5`}
+            aria-label="sidemenu button"
+            onClick={toggleSidemenu}
+          ></button>
+          <ul className=" mt-28 pl-10 flex flex-col gap-5 ">
+            <button onClick={toggleTheme} className="flex gap-1 items-center">
+              <img
+                src={darkTheme ? sunIcon : moonIcon}
+                alt="theme"
+                className="w-4 dark:w-5 dark:invert"
+              />
+              {darkTheme ? "Light" : "Dark"} Mode
+            </button>
+            <NavSignIn />
+          </ul>
         </div>
       </div>
     </nav>
